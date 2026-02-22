62abc904dc5a0fba643496c6183187495c799830
zeyad-shaban <zeyadshapan2004@gmail.com>
2026-02-22 09:33:38 +0200

    finished led controller project

--- my_pkg_py/my_pkg_py/battery_node.py
+++ my_pkg_py/my_pkg_py/battery_node.py
@@ -2,7 +2,7 @@
 import rclpy
 from rclpy.node import Node
 from example_interfaces.msg import Int32
-from my_robot_interface.srv import LedState
+from my_robot_interface.srv import SetLedState
 from enum import Enum
 
 class BatteryStates(Enum):
@@ -16,6 +16,7 @@ class BatterStatusServer(Node):
         
         self.status_hz = 10
         self.battery_state = BatteryStates.FULL
+        self.last_state_change = self.get_clock().now()
         
         self.charging_time_secs = 6
         self.discharge_time_secs = 4
@@ -23,16 +24,10 @@ class BatterStatusServer(Node):
         self.status_publisher = self.create_publisher(Int32, "battery_status", 10)
         self.create_timer(1/self.status_hz, self.cb_status_publish)
         
-        self.led_service_client = self.create_client(LedState, "/set_led")
+        self.led_service_client = self.create_client(SetLedState, "/set_led")
         
         # charge/discharge logic
-        self.discharge_timer = self.create_timer(self.discharge_time_secs, self.cb_discharged)
-        self.discharge_timer.cancel()
-        
-        self.charging_timer = self.create_timer(self.charging_time_secs, self.cb_charged)
-        self.charging_timer.cancel()
-        
-        self.cb_charged()
+        self.create_timer(1, self.cb_timer_tick)
         
     
     def cb_status_publish(self):
@@ -40,32 +35,35 @@ class BatterStatusServer(Node):
         msg.data = self.battery_state.value
         self.status_publisher.publish(msg)
     
-    def cb_charged(self):
-        self.battery_state = BatteryStates.FULL
-        self.charging_timer.cancel()
-        self.discharge_timer.reset()
+    def cb_timer_tick(self):
+        elapsed_time = (self.get_clock().now() - self.last_state_change).nanoseconds / 1e9 # typ: ignore
         
-        req = LedState.Request()
+        should_switch_to_empty = self.battery_state == BatteryStates.FULL and elapsed_time >= self.discharge_time_secs
+        should_switch_to_full = self.battery_state == BatteryStates.EMPTY and elapsed_time >= self.charging_time_secs
+        
+        if not (should_switch_to_empty or should_switch_to_full):
+            return
+        
+        self.last_state_change = self.get_clock().now()
+        req = SetLedState.Request()
         req.led_num = 3
-        req.state = False
         
+        if should_switch_to_empty:
+            self.battery_state = BatteryStates.EMPTY
+            req.state = True
+        
+        elif should_switch_to_full:
+            self.battery_state = BatteryStates.FULL
+            req.state = False
+            
+        while not self.led_service_client.wait_for_service(1):
+            self.get_logger().warning("led_service not ready...")
+            
         fut = self.led_service_client.call_async(req)
         fut.add_done_callback(self.cb_led_service)
-        
-    def cb_discharged(self):
-        self.battery_state = BatteryStates.EMPTY
-        self.discharge_timer.cancel()
-        self.charging_timer.reset()
-        
-        req = LedState.Request()
-        req.led_num = 3
-        req.state = True
-        
-        fut = self.led_service_client.call_async(req)
-        fut.add_done_callback(self.cb_led_service)
-        
+            
     def cb_led_service(self, future):
-        res: LedState.Response = future.result() # type: ignore
+        res: SetLedState.Response = future.result() # type: ignore
         self.get_logger().info(f"response success: {res.success}")
 
 
--- my_pkg_py/my_pkg_py/led_node.py
+++ my_pkg_py/my_pkg_py/led_node.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from example_interfaces.msg import String
-from my_robot_interface.srv import LedState
+from my_robot_interface.msg import LedStateArr
+from my_robot_interface.srv import SetLedState
 
 class LedNode(Node):
     def __init__(self):
@@ -10,30 +10,28 @@ class LedNode(Node):
         self.get_logger().info(f"led_node Started")
         
         self.status_hz = 8
-        self.led_status = "000"
+        self.led_state = [0, 0, 0]
 
-        self.status_publisher = self.create_publisher(String, 'led_status', 10)
+        self.status_publisher = self.create_publisher(LedStateArr, 'led_status', 10)
         self.create_timer(1/self.status_hz, self.cb_publish_status)
         
-        self.create_service(LedState, "/set_led", self.handle_set_led_state)
+        self.create_service(SetLedState, "/set_led", self.handle_set_led_state)
         
-    def handle_set_led_state(self, req: LedState.Request, res: LedState.Response):
-        if req.led_num > len(self.led_status) or req.led_num <= 0:
+    def handle_set_led_state(self, req: SetLedState.Request, res: SetLedState.Response):
+        if req.led_num > len(self.led_state) or req.led_num <= 0:
             self.get_logger().error(f"handling set led state failed, led {req.led_num} out of range")
             res.success = False
             return res
         
-        int_val =  "1" if req.state else "0"
-        idx = req.led_num - 1
-        self.led_status = self.led_status[:idx] + int_val + self.led_status[idx+1:]
+        self.led_state[req.led_num- 1] = 1 if req.state else 0
         
         res.success = True
         return res
         
         
     def cb_publish_status(self):
-        msg = String()
-        msg.data =self.led_status
+        msg = LedStateArr()
+        msg.led_state =self.led_state
         self.status_publisher.publish(msg)
 
 
